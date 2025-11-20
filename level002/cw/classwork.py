ae8134c8bf9910b7a609517b64aebedc850bf8cf
gogaasatiani <gogaasatiani123@gmail.com>
2025-11-20 21:10:41 +0400

    second commit

---
+++ level002/cw/classwork.py
@@ -0,0 +1,6 @@
+num1 = "goga"
+num2 = 5
+
+print(num1 + num2)
+
+
---
+++ level002/hw/homework3.py
@@ -0,0 +1,16 @@
+# გაასწორეთ შემდეგი კოდი:
+
+# number = 10;
+# word -> "Car'
+# print('number') # 10
+# print word
+
+# შესასწორებელია:
+# word -> "car" უნდა იყოს -> word = "car"
+# შემდეგ print('number') რადგან ეს დაბეჭდავს პირდაპირ სტრინგს(number)-ს
+# და შემდეგ print(word) უნდა იყოს ბრჩხილებში
+# სწორი ვარიანტია 
+number = 10
+word = "car"
+print(number)
+print(word)
