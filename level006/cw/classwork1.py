5f7557219498155382d7c0f6845f220e04b9631e
gogaasatiani <gogaasatiani123@gmail.com>
2025-11-22 17:13:31 +0400

    sixth commit

---
+++ level006/cw/classwork1.py
@@ -0,0 +1,5 @@
+#  input - ის გამოყენებით მომხმარებელს ვაძლევთ უფლებას შეიყვანოს მისი ინფორმაცია და შემდეგ შევინახოთ ხოლო ოუთფუთს გამოაქვს ის  შეყვანილი ინფორმაცია რაც მომხმარებელმა შეიყვანა ის მაგალითად:
+name = input("write your name here pls: ")
+surname = input("write you surname here pls: ")
+print(name + " " + surname)
+
---
+++ level006/cw/classwork2.py
@@ -0,0 +1,5 @@
+# მომხმარებელს შემოატანინეთ სახლი, გვარი და ასაკი, თითოეული მათგანი შეინახეთ ცვლადებში(დაგჭირდებათ 3 ცვლადი). ამ ცვლადების გამოყენებით შექმენით წინადადება და დაბეჭდეთ terminal-ში(წინადადების შესაქმნელად დაგჭირდებათ კონკადინახია)
+
+your_name = input("pls enter your name here: ")
+your_surname = input("pls enter your surname here: ")
+print("your name is " + your_name + " your surname is " + your_surname )
