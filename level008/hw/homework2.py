bdadf96107888cfa53c02a4b3106ace0363fe958
gogaasatiani <gogaasatiani123@gmail.com>
2025-11-23 15:55:31 +0400

    eight commit

---
+++ level008/hw/homework2.py
@@ -0,0 +1,49 @@
+# მომხმარებელს შემოატანინეთ 5 რიცხვი და გაიგეთ მათი საშუალო არითმეტიკული
+# დავალება2
+# user1 = int(input("please enter the number that you want it to be outputed: "))
+# user2 = int(input("please enter the number that you want it to be outputed: "))
+# user3 = int(input("please enter the number that you want it to be outputed: "))
+# user4 = int(input("please enter the number that you want it to be outputed: "))
+# user5 = int(input("please enter the number that you want it to be outputed: "))
+
+# print(user1 + user2 + user3 + user4 + user5 / 5)
+
+# დავალება3
+# კომენტარების სახით ჩამოწერეთ ყველა ნასწავლი მონაცემთა ტიპი. დაახასიათეთ ისინი და თითოეულზე მოიყვანეთ 3-3 მაგალითი
+
+# input,int,print,variable,debugging,concadination,html,body,head,
+
+# input - ის მაგალითები:
+# user1 = input("please enter your name")
+# user2 = input("please enter your name")
+# user3 = input("please enter your name")
+
+# int - ის მაგალითები:
+# user1 = int(input("please enter number: "))
+# user2 = int(input("please enter number: "))
+# user3 = int(input("please enter number: "))
+
+# print - ის მაგალითები
+# print("goga")
+# print("gaga")
+# print("gio")
+
+# debbuging გვეხმარება რომ ვიპოვოთ კოდში შეცდომა
+
+# concadination - ნიშნავს სტრინგის სტრინგე შეერთება როგორიცაა: 
+# user1 = "goga"
+# user2 = "gaga"
+# print(user1 + user2)
+
+# user1 = "gabrieli"
+# user2 = "sandro"
+# print(user1 + user2)
+
+# user1 = "data"
+# user2 = "dachi"
+# print(user1 + user2)
+
+
+# html - არის საიტის მთავარი თეგი სადაც იწერება სხვა დანარჩენი კოდი.
+# body - ნებისმიერი რამ, რაც საიტზე უნდა გამოჩნდეს იწერება მის შიგნით.
+# head - გამოიყენება საიტის შესახებ ინფორმაციის ბრაუზერისთვის მიწოდებას
