7b4f0c8275de64e2df6685cf3622b4613135c26d
gogaasatiani <gogaasatiani123@gmail.com>
2025-11-24 21:30:36 +0400

    tenth commit

---
+++ level010/cw/classwork4.py
@@ -0,0 +1,22 @@
+# მოიძიეთ ინფორმაცია if/elif/else-ებზე და შეეცადეთ გააკეთოთ რამოდენიმე მაგალითი.
+
+a = 33
+b = 33
+if b > a:
+    print("b > a")
+elif a == b:
+    print("a = b")
+
+a = 13
+b = 28
+if a < b:
+    print("b > a")
+else:
+    print("a > b")
+
+i = 190
+H = 67
+if i < H:
+    print("i < H")
+elif i > H:
+    print("i > H")
